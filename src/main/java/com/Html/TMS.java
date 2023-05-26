f1664bb8641cb624430846ce996f5c26f21fa670
Михаил <Wnuk1988@mail.ru>
2023-05-26 15:01:49 +0300

    Initial commit!

---
+++ src/main/java/com/Html/TMS.java
@@ -0,0 +1,16 @@
+package com.Html;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/tms")
+public class TMS extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/pages/TMS.html").forward(req, resp);
+    }
+}
