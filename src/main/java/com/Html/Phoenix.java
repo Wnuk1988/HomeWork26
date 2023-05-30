a59bae393f72dc121eb17e85e87641f36856f52d
Михаил <Wnuk1988@mail.ru>
2023-05-30 13:49:46 +0300

    Changing the location of folders.

--- src/main/java/com/Html/Phoenix.java
+++ src/main/java/com/Html/Phoenix.java
@@ -10,6 +10,6 @@ import java.io.IOException;
 public class Phoenix extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/pages/Phoenix.html").forward(req, resp);
+        req.getRequestDispatcher("/Phoenix.html").forward(req, resp);
     }
 }
--- src/main/java/com/Html/TMS.java
+++ src/main/java/com/Html/TMS.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class TMS extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/pages/TMS.html").forward(req, resp);
+        req.getRequestDispatcher("/TMS.html").forward(req, resp);
     }
 }
